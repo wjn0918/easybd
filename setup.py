18b22f8e639ef6ee3e52276099f25527e3e7a9c4
wjn0918 <wjn_0918@163.com>
2024-12-05 12:10:46 +0800

    add dependencies

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
     # 指定包名，即你需要打包的包名称，要实际在你本地存在哟，它会将指定包名下的所有"*.py"文件进行打包哟，但不会递归去拷贝所有的子包内容。
     # 综上所述，我们如果想要把一个包的所有"*.py"文件进行打包，应该在packages列表写下所有包的层级关系哟~这样就开源将指定包路径的所有".py"文件进行打包!
     packages=find_packages(where=".", include=["easybd.*"]),
-    # install_requires=["xlsxwriter>=3.1.1", "pyspark"]
+    install_requires=["xlsxwriter>=3.1.1","pandas==1.4.4","SQLAlchemy==2.0.36",]
 )
