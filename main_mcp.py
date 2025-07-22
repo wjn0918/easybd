d413d3c10b4a851ba11e1e251e6ea943b73458d3
wjn0918 <wjn_0918@163.com>
2025-07-22 17:24:16 +0800

    mcp

---
+++ main_mcp.py
@@ -0,0 +1,40 @@
+from typing import Union
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.staticfiles import StaticFiles
+from fastapi_mcp import FastApiMCP
+
+from db import create_db_and_tables
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+mcp = FastApiMCP(app)
+# Mount the MCP server directly to your FastAPI app
+mcp.mount()
+
+from api import config, tools, db, deploy_code, excel_api, file_api, database_api
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+app.include_router(config.router)
+app.include_router(tools.router)
+app.include_router(db.router)
+app.include_router(deploy_code.router)
+app.include_router(excel_api.router)
+app.include_router(file_api.router)
+app.include_router(database_api.router)
+
+mcp.setup_server()
